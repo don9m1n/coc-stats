fa65f0040cf059fb236e61db0ed15c5e958a110c
Dongmin Kim <ddmkim94@gmail.com>
2023-12-09 12:34:56 +0900

    create project

---
+++ src/main/java/com/dmk/cocstats/CocStatsApplication.java
@@ -0,0 +1,13 @@
+package com.dmk.cocstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dmk/cocstats/CocStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmk.cocstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
