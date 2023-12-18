4a2679b88fc8a9aabd5e1b2c1450478a7f856ae2
Dongmin Kim <ddmkim94@gmail.com>
2023-12-18 12:15:06 +0900

    Feat: 회원 도메인 구현

---
+++ src/main/java/com/dmk/cocstats/domain/member/model/MemberRole.java
@@ -0,0 +1,14 @@
+package com.dmk.cocstats.domain.member.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum MemberRole {
+
+    MEMBER("ROLE_MEMBER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String role;
+}
